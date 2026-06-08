7aa678a798f0c7883e0b756ef0338ad5bf053883
Amit Seal Ami <lordamit@gmail.com>
2026-06-08 16:51:42 -0400

    ignored in lint

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -21,7 +21,7 @@ export default defineConfig([
     },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'error',
       'prefer-const': 'warn',
       'react-refresh/only-export-components': 'off',
     },
